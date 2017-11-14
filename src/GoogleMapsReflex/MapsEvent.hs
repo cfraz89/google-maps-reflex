6acde06aab34f9e0b5109e81629d0951b309ac39
Crusher <chris@zova.com>
2017-11-14 15:11:20 +1100

    make mapsevent accept selector as well as instance to allow events on non-map vals

--- src/GoogleMapsReflex/MapsEvent.hs
+++ src/GoogleMapsReflex/MapsEvent.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module GoogleMapsReflex.MapsEvent where
+module GoogleMapsReflex.MapsEvent(mapsEvent, addListenerEvent, MapsEvent(..)) where
     
 import Data.Text
 import Language.Javascript.JSaddle.Value
@@ -17,10 +17,12 @@ mapsEventText MouseMove = "mousemove"
 instance ToJSVal MapsEvent where
     toJSVal e = val $ mapsEventText e
 
-mapsEvent :: (MonadWidget t m) => MapsEvent -> GoogleMaps t e -> m (Event t JSVal)
-mapsEvent eventType (GoogleMaps state) = performAddListener (mapsEventText eventType) mapEvent
-    where mapEvent = push (\s -> return (_mapsState_mapVal s)) (updated state)
-          performAddListener :: (MonadWidget t m) => Text -> Event t JSVal -> m (Event t JSVal)
-          performAddListener eventName mapEvent = performEventAsync (unReturn <$> (addListener eventName <$> mapEvent))
-          unReturn :: (Monad m) => (a -> m v) -> a -> m () --Until i can figure out what to do with the listener ref
+mapsEvent :: (MonadWidget t m) => MapsEvent -> GoogleMaps t e -> (MapsState e -> Maybe JSVal) -> m (Event t JSVal)
+mapsEvent eventType (GoogleMaps state) selector = addListenerEvent eventType mapEvent
+    where mapEvent = push (\s -> return (selector s)) (updated state)
+
+addListenerEvent :: (MonadWidget t m) => MapsEvent -> Event t JSVal -> m (Event t JSVal)
+addListenerEvent eventType mapEvent = performEventAsync (unReturn <$> (addListener eventName <$> mapEvent))
+    where unReturn :: (Monad m) => (a -> m v) -> a -> m () --Until i can figure out what to do with the listener ref
           unReturn f a = f a >> return ()
+          eventName = mapsEventText eventType
